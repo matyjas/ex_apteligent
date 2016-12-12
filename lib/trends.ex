712e90bde838254da304c5553ce447b72b000944
Maciej Matyjas <matyjas@gmail.com>
2016-12-12 21:02:45 +0000

    fixed text formatting

--- lib/trends.ex
+++ lib/trends.ex
@@ -1,15 +1,14 @@
 defmodule Trends do
 
-	def trends(data_loc, app_id, token, 
-						 apteligent_client \\ ApteligentClientHttp) do
-		apteligent_client.request(data_loc, "/trends/", app_id, token)
-	end
+  def trends(data_loc, app_id, token, apteligent_client \\ ApteligentClientHttp) do
+    apteligent_client.request(data_loc, "/trends/", app_id, token)
+  end
 
-	def dau(appId) do
-		1_000
-	end
+  def dau(appId) do
+    1_000
+  end
 
-	def mau(appId) do
-		1_000_000
-	end
+  def mau(appId) do
+    1_000_000
+  end
 end
