b01d2044f0d9b0b9a27bc1d02fa3e0a1851f0483
Maciej Matyjas <matyjas@gmail.com>
2016-12-12 21:07:49 +0000

    cleaned up text formatting

--- test/trends_test.exs
+++ test/trends_test.exs
@@ -1,11 +1,10 @@
 defmodule TrendsTest do
   use ExUnit.Case
   alias ExApteligent.Trends
-	doctest ExApteligent.Trends
+  doctest ExApteligent.Trends
 
-	test "trends has data" do
-		{:ok, data} = Trends.trends(:us, "app-1", "token-2")
-		assert data != nil
-	end
-
+  test "trends has data" do
+    {:ok, data} = Trends.trends(:us, "app-1", "token-2")
+    assert data != nil
+  end
 end
