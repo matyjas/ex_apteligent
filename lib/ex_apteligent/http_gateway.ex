5779c35b3255930b53d042fdfe8e96c8c2bafca1
Maciej Matyjas <matyjas@gmail.com>
2016-12-15 22:11:16 +0000

    that was the big one

--- lib/ex_apteligent/http_gateway.ex
+++ lib/ex_apteligent/http_gateway.ex
@@ -1,7 +1,21 @@
 defmodule ExApteligent.HttpGateway do
+  import ExApteligent.UrlBuilder, only: [prepare: 3]
+  alias HTTPotion.Response
+  
   @behaviour ApteligentGateway
   def request(data_loc, path, app_id, token) do
-    {:error, "for now"}
+    url = prepare(path, app_id, data_loc: data_loc)
+    #     HTTPotion.get(url, [headers: ["Authorization": "Bearer 5a839540a09f12373e52c7c82680318e"]])
+    HTTPotion.get(url, [headers: ["Authorization": "Bearer " <> token]])
+    |> make_presentable
+  end
+
+  defp make_presentable(resp = %Response{status_code: 200}) do
+    {:ok, resp.body}
+  end
+
+  defp make_presentable(%Response{status_code: status_code}) do
+    {:error, status_code}
   end
 end
 
--- test/http_gateway_test.exs
+++ test/http_gateway_test.exs
@@ -6,11 +6,18 @@ defmodule HttpGatewayTest do
   @moduletag :integration
   
   test "request with bad token should be an :error" do
-    {:error, resp} = HttpGateway.request(:us, "trends", "519d53101386202089000007", "token")
+    {status, _} = HttpGateway.request(:us, "trends", "519d53101386202089000007", "token")
+    assert status == :error
   end
 
   test "there should be an OAuth token available" do
     token = System.get_env "APTELIGENT_TOKEN"
     assert token != nil
+  end
+
+  test "request with good token should be :ok" do
+    token = System.get_env("APTELIGENT_TOKEN")
+    {status, _} = HttpGateway.request(:us, "trends", "519d53101386202089000007", token)
+    assert status == :ok
   end
 end
