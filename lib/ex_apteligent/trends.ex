7f86e8bd62ec25519b793b6f0bb9e69b08d9669c
Maciej Matyjas <matyjas@gmail.com>
2016-12-15 22:32:46 +0000

    working client for trends endpoints

--- lib/ex_apteligent/trends.ex
+++ lib/ex_apteligent/trends.ex
@@ -2,15 +2,19 @@ defmodule ExApteligent.Trends do
   alias ExApteligent.HttpGateway
   
   def trends(data_loc, app_id, token, gateway \\ HttpGateway) do
-    {:ok, trends} = gateway.request(data_loc, "/trends/", app_id, token)
-    JsonParser.parse trends
+    req(data_loc, "trends", app_id, token, gateway)
   end
 
-  def dau(appId) do
-    1_000
+  def dau(data_loc, app_id, token, gateway \\ HttpGateway) do
+    req(data_loc, "trends/dau", app_id, token, gateway)
   end
 
-  def mau(appId) do
-    1_000_000
+  def mau(data_loc, app_id, token, gateway \\ HttpGateway) do
+    req(data_loc, "trends/mau", app_id, token, gateway)
+  end
+
+  defp req(data_loc, path, app_id, token, gateway) do
+    {:ok, data} = gateway.request(data_loc, path, app_id, token)
+    JsonParser.parse data
   end
 end
