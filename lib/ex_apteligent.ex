01cc2d13cf377ffbfd59861dbb9fa6ee835f1aba
Maciej Matyjas <matyjas@gmail.com>
2016-12-19 21:56:10 +0000

    hello documentation

--- lib/ex_apteligent.ex
+++ lib/ex_apteligent.ex
@@ -1,15 +1,30 @@
 defmodule ExApteligent do
   alias ExApteligent.HttpGateway
+	@moduledoc """
+	The parent module for this Apteligent client library, it
+  wraps implemented API endpoints as sub-modules. 
+  see `ExApteligent.Trends`
+  """
   
   defmodule Trends do
+		@moduledoc """
+    Supports accessing values from Apteligent's trends endpoints
+    """
+		@doc """
+		Returns massive map of lots of trends information
+    """
     def trends(app_id, token, gateway \\ HttpGateway, options \\ []) do
       gateway.request("trends", app_id, token, options)
     end
-
+		@doc """
+    Returns map containing daily active users count
+    """
     def dau(app_id, token, gateway \\ HttpGateway, options \\ []) do
       gateway.request("trends/dau", app_id, token, options)
     end
-
+		@doc """
+		Returns map containing monthly active users count
+    """
     def mau(app_id, token, gateway \\ HttpGateway, options \\ []) do
       gateway.request("trends/mau", app_id, token, options)
     end
